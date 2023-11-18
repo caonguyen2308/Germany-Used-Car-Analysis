f034baa4578302699dab94bed7fdbb671e6eeacf
Cao Hoàng Khôi Nguyên <100473342+caonguyen2308@users.noreply.github.com>
2023-11-19 01:52:15 +0700

    newupdatecsvfile

--- Final.py
+++ Final.py
@@ -5,11 +5,12 @@ from matplotlib import pyplot as plt
 import ipywidgets as widgets
 import os
 import warnings
+import re
 
 df = pd.read_csv('D:/Cybersoft/Germany-Used-Car-Analysis/data.csv', encoding='ISO-8859-1')
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
-    df['registration_date'] = pd.to_datetime(df['registration_date'], errors='coerce')
+
 # Clean data
 # fillna values by mean (type int only)
 numeric_columns = df.select_dtypes(include=[np.number]).columns
@@ -19,13 +20,55 @@ df[numeric_columns] = df[numeric_columns].fillna(df[numeric_columns].mean())
 df['registration_date'] = pd.to_datetime(df['registration_date'], errors='coerce')
 df['registration_date'] = df['registration_date'].dt.strftime('%Y-%m-%d')
 
-# Save new file csv
-output_directory = 'D:/Cybersoft/Final-Test-Cybersoft'
-os.makedirs(output_directory, exist_ok=True)
-output_file_path = os.path.join(output_directory, 'final_cleaned_data.csv')
-df.to_csv(output_file_path, index=False)
+# Delete Duplicate 
+df = df.drop_duplicates()
 
-print(df)
+
+# Clean column with has (l/km) and (g/km)
+def clean_and_convert(value):
+    if isinstance(value, str) and not value.isdigit():
+        numeric_value = ''.join(filter(str.isdigit, value))
+        return int(numeric_value) if numeric_value else None
+    elif isinstance(value, str) and value.isdigit():
+        return int(value)
+    else:
+        return value
+def clean_and_convert(value):
+    if isinstance(value, str):
+        numeric_part = ''.join(filter(lambda x: x.isdigit() or x in ['.', ','], value))
+        numeric_part = numeric_part.replace(',', '.')  
+        try:
+            float_value = float(numeric_part)
+            rounded_value = round(float_value, 1)  
+            return rounded_value
+        except ValueError:
+            return None
+    else:
+        return value
+
+# Apply the cleaning function to the column
+df['fuel_consumption_g_km'] = df['fuel_consumption_g_km'].apply(clean_and_convert)
+df['fuel_consumption_l_100km'] = df['fuel_consumption_l_100km'].apply(clean_and_convert)
+
+# Fill NaN value by the mean of column fuel_consumption_g_km
+df['fuel_consumption_g_km'] = pd.to_numeric(df['fuel_consumption_g_km'], errors='coerce') 
+mean_fuel_consumption_g_km = df['fuel_consumption_g_km'].mean()  
+df['fuel_consumption_g_km'].fillna(mean_fuel_consumption_g_km, inplace=True)
+
+# Fill NaN value by the mean of column fuel_consumption_l/km
+df['fuel_consumption_l_100km'] = pd.to_numeric(df['fuel_consumption_l_100km'], errors='coerce') 
+mean_fuel_consumption_l_km = df['fuel_consumption_l_100km'].mean()  
+df['fuel_consumption_l_100km'].fillna(mean_fuel_consumption_l_km, inplace=True)
+df = df[df['fuel_consumption_l_100km'] <= 20]
+
+# Print column
+print(df['fuel_consumption_g_km'])
+print(df['fuel_consumption_l_100km'])
+
+# Save new file csv
+new_cleaned_data = df.to_csv('D:/Cybersoft/Germany-Used-Car-Analysis/final_cleaned_data.csv', index = True) 
+print('\nnew_cleaned_data\n', new_cleaned_data) 
+
 
 
 
